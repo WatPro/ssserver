788b8a1f15187353389c81d1440d6da8731e8906
WatPro <31579668+WatPro@users.noreply.github.com>
2018-05-27 20:14:58 +0800

    Update RASPrun.sh

--- RASPrun.sh
+++ RASPrun.sh
@@ -77,7 +77,7 @@ sudo iptables --table nat --append SHADOWSOCKS --destination 224.0.0.0/4 --jump 
 sudo iptables --table nat --append SHADOWSOCKS --destination 240.0.0.0/4 --jump RETURN
 sudo iptables --table nat --append SHADOWSOCKS --protocol tcp --jump REDIRECT --to-ports 1080
 sudo iptables --table nat --append OUTPUT --out-interface ${DEV} --protocol tcp --jump SHADOWSOCKS
-sudo iptables --table nat --append OUTPUT --physdev-out ${DEV} --protocol tcp --jump SHADOWSOCKS
+sudo iptables --table nat --append OUTPUT --match physdev --physdev-out ${DEV} --protocol tcp --jump SHADOWSOCKS
  
 # Add any UDP rules
 if [ ! -n "`ip route show table 100`" ] 
